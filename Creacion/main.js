38e484934746e46ec6d00231b36fdbd38666907a
Daniela Correa <correadaniela783@gmail.com>
2023-02-11 14:17:18 -0500

    listo mi boton

--- Creacion/main.js
+++ Creacion/main.js
@@ -1,1 +1,6 @@
-const  navToggle = document.querySelector("button")
+const  navToggle = document.querySelector(".nav-toggle");
+const navMenu = document.querySelector(".menu");
+
+navToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("visible")
+})
